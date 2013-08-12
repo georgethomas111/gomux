b7151735069e1833c1a917764f07dda206725b7e
george <georgethomas.mec@gmail.com>
2013-08-13 00:56:59 +0530

    Adding the print character feature

--- src/gomux/gomux.go
+++ src/gomux/gomux.go
@@ -3,22 +3,22 @@ package gomux
 import (
 	"fmt"
 	"termbox-go"
+	"strconv"
 )
 
 type RunDetails struct {
-
-NoOfPanes int
-Width int
-Height int
-
+	NoOfPanes int
+	Width     int
+	Height    int
 }
 
 var runDetails RunDetails
-var inputChan = make(chan termbox.Key, 100)
+var inputChan = make(chan termbox.Event, 100)
+var processChan = make(chan string, 100)
 
 //
 // initializes gomux with the number of panes to be shown in the output.
-// 
+//
 //
 
 func Init(noPanes int) error {
@@ -35,38 +35,55 @@ func Init(noPanes int) error {
 	return err
 }
 
+
+func ProcessCommands() {
+	for {
+		command := <-processChan
+		fmt.Println("$" + command)
+	}
+}
+
 //
 // Takes inputs and finally runs them when an enter key is hit.
 //
 //
 
 func GetInput() {
-
-key := <-inputChan
-switch key {
-	case termbox.KeyEnter :
-		fmt.Println("\n\n Enter key hit")
+	buffer := ""
+	for {
+		event := <-inputChan
+		switch event.Key {
+		case termbox.KeyEnter:
+			processChan <- buffer
+			buffer = ""
+		default:
+			// TODO 
+			// 1. Decide on how to handle the errors.
+			// 2. Decide on how to handle spaces.
+			ch, _  := strconv.Unquote(strconv.QuoteRuneToASCII(event.Ch))
+			buffer += ch
+		}
 	}
 }
 
 func Run() error {
 
-go GetInput()
-
+	go GetInput()
+	go ProcessCommands()
 loop:
 	for {
 		event := termbox.PollEvent()
 		switch event.Type {
-		case termbox.EventKey :
+		case termbox.EventKey:
 			switch event.Key {
 			case termbox.KeyEsc:
 				break loop
 			default:
-				inputChan <- event.Key
+				inputChan <- event
 			}
-			case termbox.EventError :
-				return event.Err
+		case termbox.EventError:
+			return event.Err
 		}
 	}
-return nil
+	return nil
 }
